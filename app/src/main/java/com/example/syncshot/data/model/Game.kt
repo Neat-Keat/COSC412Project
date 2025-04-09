9e7727a64cc7dd55f480306e0d2c06403b71f38b
D-Pais <dpais1@students.towson.edu>
2025-04-09 17:54:21 -0400

    Updated Game.kt constructor and included score math
    - renamed "holes" to "strokes" for clarity
    - changed all val's (except "val id") into var's, for when user manually updates games
    - added to the primary constructor body to initialize scores and finalScores arrays with proper values
    - date and location can now be passed as null, for if user gives no info

--- app/src/main/java/com/example/syncshot/data/model/Game.kt
+++ app/src/main/java/com/example/syncshot/data/model/Game.kt
@@ -9,12 +9,30 @@ import androidx.room.TypeConverters
 //This is the custom data type we are using to store info related to every golf game entry
 @Entity(tableName = "games")
 @TypeConverters(Converters::class)
+
+//Primary Constructor takes, in order: id, names array, strokes 2D array, par array, date (put null if user doesn't provide), location (put null if user doesn't provide)
 data class Game(
     @PrimaryKey
     val id: String,         //Unique identifier for each game, kind of like an index for the database
-    val location: String,   //Stores the location of where the game took place
-    val date: String,       //Stores the date that the game was played
-    val names: Array<String>,     //Stores each player's name by index
-    val holes: Array<IntArray>,   //Stores an int array of scores for each player
-    val finalScores: IntArray     //Stores the final scores of each player
-)
+    var names: Array<String>,       //Stores each player's name
+    var strokes: Array<IntArray>,   //Stores an int array of shots taken on each hole, for each player by index
+	var par: IntArray,      //Stores the Par for each hole
+	var date: String?,      //Stores the date that the game was played. can be null, for if user gives nothing
+    var location: String?   //Stores the location of where the game took place. can be null too
+) {
+    var scores = Array(strokes.size){IntArray(18){0}}    //Stores the compared-to-par scores of each player, for each hole.  Initialized to all 0s, as though a player stroked Par every hole
+    var finalScores = IntArray(strokes.size){0}          //Stores the final scores of each player
+    init {
+        for (p in 0 until scores.size) {
+    		for(i in 0..17) {
+                //if the number of strokes on a given hole is <= 0 (e.g. an invalid number) then it assumes they got Par on that hole
+    			if (strokes[p][i] > 0) {
+                    scores[p][i] = strokes[p][i] - par[i]
+                }
+    		}
+    	}
+    	for (p in 0 until finalScores.size) {
+    		finalScores[p] = scores[p].sum()
+    	}
+    }
+}
